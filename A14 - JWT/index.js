899ed7f8e48e4ad14021d50830281b1a66c39f9b
Eduardo Castro <eduardofc.dev@gmail.com>
2023-06-20 10:36:51 -0300

    Aula 14

---
+++ A14 - JWT/index.js
@@ -0,0 +1,21 @@
+const http = require('http'); 
+const express = require('express'); 
+const app = express(); 
+ 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+ 
+app.get('/', (req, res, next) => {
+    res.json({message: "Tudo ok por aqui!"});
+})
+ 
+app.get('/clientes', (req, res, next) => { 
+    console.log("Retornou todos clientes!");
+    res.json([{id:1,nome:'Fulano'}]);
+}) 
+ 
+const server = http.createServer(app); 
+server.listen(3000);
+console.log("Servidor escutando na porta 3000...");
+
+//https://www.luiztools.com.br/post/autenticacao-json-web-token-jwt-em-nodejs/
